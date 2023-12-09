212594d016f2d9c3b544738b610625ca4f95a22c
adi_walture <adityawalture9298@gmail.com>
2023-12-09 19:49:41 +0530

    pubspec.yaml dependencies ver updated

--- lib/Pages/authPage.dart
+++ lib/Pages/authPage.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_final_fields, use_build_context_synchronously, unused_local_variable
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lottie/lottie.dart';
