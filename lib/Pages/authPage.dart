7fcc6e263e1cae19001b30823130e4c5437f6fab
adi_walture <adityawalture9298@gmail.com>
2023-12-31 19:20:55 +0530

    screenShot added

--- lib/Pages/authPage.dart
+++ lib/Pages/authPage.dart
@@ -5,7 +5,7 @@ import 'dart:io';
 import 'package:chatify/Pages/loadingScreen.dart';
 import 'package:chatify/widgets/user_image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
--- lib/Pages/chatScreen.dart
+++ lib/Pages/chatScreen.dart
@@ -1,7 +1,7 @@
 import 'package:chatify/widgets/chat_messages.dart';
 import 'package:chatify/widgets/new_messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
